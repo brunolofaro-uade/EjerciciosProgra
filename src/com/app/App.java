7e145a92910cd0d945250453820ebd12f80d0a82
BrunoLoFaro <lofarobruno@gmail.com>
2023-03-17 13:14:52 -0300

    recursividad-sumar-digitos

--- src/com/app/App.java
+++ src/com/app/App.java
@@ -4,8 +4,7 @@ import com.clase1.*;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print(EjerciciosClase.EsPrimo(2));
+		System.out.print(EjerciciosClase.SumarDigitosRec(23));
 	}
 
 }
--- src/com/clase1/EjerciciosClase.java
+++ src/com/clase1/EjerciciosClase.java
@@ -31,4 +31,28 @@ public class EjerciciosClase {
 			}
 		}
 	}
+	
+	public static int GetCantidadDigitos(int num) {
+		if(num==0)
+			num++;
+		return (int)Math.floor(Math.log10(Math.abs(num))+1);
+	}
+	
+	public static long SumarDigitosFor(double num) {
+		String strNum = Double.toString(num);
+		long res = 0;
+		for(int i =0 ; i<strNum.length(); i++) {
+			char caracter = strNum.charAt(i);
+			if((int)caracter >= 48 && (int)caracter <=57) {		
+				num = Math.abs(Character.getNumericValue(caracter));
+				res += num;
+			}
+		}
+		return res;
+	}
+	public static int SumarDigitosRec(int num) {
+		if(num == 0)
+			return num;
+		return num%10 + SumarDigitosRec(num/10);
+	}
 }
