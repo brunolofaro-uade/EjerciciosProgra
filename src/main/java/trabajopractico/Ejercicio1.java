b6b60cc23d5656310876380063f265ba4e14a18b
Dalla Valle Horacio Daniel Jesus <dallavalle.h@pm.me>
2023-04-03 20:05:56 -0300

    Create the stub file to solve the first TP

---
+++ src/main/java/trabajopractico/Ejercicio1.java
@@ -0,0 +1,11 @@
+/**
+ * Consignas:
+ * 1. Dada una cola de N pilas con N elementos (entonces tendra forma matricial), calcular la traza.
+ * 2. Calcular la traspuesta
+ */
+
+package trabajopractico;
+
+public class Ejercicio1 {
+
+}
